4d7dcd37dabc993498ec33574aa6e43e7e3aa637
Metallifax <mathew_forgione@msn.com>
2022-02-22 22:26:46 -0400

    Added some initial javadocs to both stub and test file.

--- src/main/java/Greeter.java
+++ src/main/java/Greeter.java
@@ -1,6 +1,20 @@
-class Greeter {
+/**
+ * This is the Main class of the Greeter exercise. In computer science, we think of
+ * <a href="https://docs.oracle.com/javase/tutorial/java/javaOO/classes.html#PageTitle">classes</a>
+ * as the blueprints of an object, something we'll talk more about as the track progresses.
+ * @see Class
+ */
+public class Greeter {
 
-    String getGreeting() {
+    /**
+     * In Java classes, we use <a href="https://docs.oracle.com/javase/tutorial/java/javaOO/methods.html#PageTitle">methods</a>
+     * to create functionality inside our classes so that our objects can do useful things, like make calculations,
+     * parse strings or even say "Hello, World!"
+     * <p>Enter some text between the quotation marks after the {@code return} token to get started.</p>
+     * @return "Hello, World!" as a {@code String}
+     * @see String
+     */
+    public String getGreeting() {
         return "Hello, World!";
     }
 
--- src/test/java/GreeterTest.java
+++ src/test/java/GreeterTest.java
@@ -2,8 +2,18 @@ import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+/**
+ * This is the test class where you can run tests against your code file.
+ */
 public class GreeterTest {
 
+    /**
+     * Each test method checks a certain aspect of your code to see if it meets the requirements of the exercise.
+     * <p>If you're unsure how to run a test in Gradle, check out this article in
+     * <a href="https://exercism.org/docs/tracks/java/tests">our Java documentation</a> to learn how.</p>
+     * Alternatively, you can use your favourite IDE to run your test automatically, like in
+     * <a href="https://www.jetbrains.com/help/idea/work-with-tests-in-gradle.html#run_gradle_test">IntelliJ IDEA</a>.
+     */
     @Test
     public void testThatGreeterReturnsTheCorrectGreeting() {
         assertThat(new Greeter().getGreeting()).isEqualTo("Hello, World!");

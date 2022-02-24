b878c404a3b34322aca5a09383490c3bab75a6a6
Metallifax <mathew_forgione@msn.com>
2022-02-23 20:29:42 -0400

    Simplified the stub file javadocs.

--- src/main/java/Greeter.java
+++ src/main/java/Greeter.java
@@ -1,18 +1,13 @@
 /**
- * This is the Main class of the Greeter exercise. In computer science, we think of
- * <a href="https://docs.oracle.com/javase/tutorial/java/javaOO/classes.html#PageTitle">classes</a>
- * as the blueprints of an object, something we'll talk more about as the track progresses.
- * @see Class
+ * Main class of the Greeter exercise which holds our {@code getGreeting()} method.
  */
 public class Greeter {
 
     /**
-     * In Java classes, we use <a href="https://docs.oracle.com/javase/tutorial/java/javaOO/methods.html#PageTitle">methods</a>
-     * to create functionality inside our classes so that our objects can do useful things, like make calculations,
-     * parse strings or even say "Hello, World!"
-     * <p>Enter some text between the quotation marks after the {@code return} token to get started.</p>
+     * In Java, we write <a href="https://docs.oracle.com/javase/tutorial/java/javaOO/methods.html#PageTitle">methods</a>
+     * so that our classes can do useful things, like make calculations, parse strings or say a greeting for the world!
+     * <p>Type some text between the quotation marks after the {@code return} token to get started writing Java code!</p>
      * @return "Hello, World!" as a {@code String}
-     * @see String
      */
     public String getGreeting() {
         return "Hello, World!";
